7a0bae6413a56aac7ecec87fac5a39ad5c95b5c5
Park Junbeom <108970338+beom090@users.noreply.github.com>
2023-10-18 20:29:31 +0900

    qr 페이지 제작
    서버에서 qr 값 받아오기 제작
    공지사항 페이지 초안 제작

--- src/qr/Qr.js
+++ src/qr/Qr.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import QRCode from "qrcode.react";
+import MenuBar from "../main/MenuBar";
+import axios from "axios";
+import "./Qr.css";
 
 function Qr() {
-  const today = new Date();
-  const formattedDate = `${today.getFullYear()}-${
-    today.getMonth() + 1
-  }-${today.getDate()}`;
+  const [accessToken, setAccessToken] = useState("");
+  const [qrCodeValue, setQRCodeValue] = useState("");
 
-  const dateData = {
-    year: today.getFullYear(),
-    month: today.getMonth() + 1,
-    day: today.getDate(),
-  };
+  useEffect(() => {
+    const storedAccessToken = localStorage.getItem("Access-Token");
+    setAccessToken(storedAccessToken);
+  }, []);
 
-  const dateJson = JSON.stringify(dateData);
+  useEffect(() => {
+    if (accessToken) {
+      const data = {
+        courseId: 5,
+        academyId: 3,
+        memberId: 7,
+      };
+
+      console.log("accessToken:", accessToken);
+
+      axios
+        .post("/api/attendance/generate/qrcode", data, {
+          headers: { "Access-Token": accessToken },
+        })
+        .then((response) => {
+          const { date } = response.data;
+          setQRCodeValue(date);
+        })
+        .catch((error) => {
+          console.error("Error generating QR code:", error);
+          // 에러 처리
+        });
+    }
+  }, [accessToken]);
 
   return (
-    <div
-      className="QrCode"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <QRCode value={dateJson} size={200} />
+    <div className="check">
+      <MenuBar />
+      <div className="QrContainer">
+        <div className="QrWrapper">
+          <QRCode size={200} value={qrCodeValue} />
+        </div>
+      </div>
     </div>
   );
 }
