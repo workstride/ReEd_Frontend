dc2a3278573e7ab1abda4967d7385e4ff8f13616
Park Junbeom <108970338+beom090@users.noreply.github.com>
2023-11-05 17:29:50 +0900

    토큰 재발급 추가 & 공지사항 디자인 변경 완료
    토큰 재발급 로직 추가 & 공지사항 디자인 완료

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Main.css";
 import MenuBar from "./MenuBar";
+import axios from "axios";
 
 function Main() {
+  const [schoolList, setSchoolList] = useState([]);
+
+  const fetchSchoolList = async () => {
+    try {
+      const response = await axios.get("/api/schools");
+      const data = response.data;
+      setSchoolList(data);
+    } catch (error) {
+      console.error("Error fetching school list:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSchoolList();
+  }, []);
+
   return (
-    <div className="mainContainer">
+    <div className="mainContainer" style={{ color: "white" }}>
       <MenuBar />
       <button className="addSchoolButton">등록</button>
+      <div className="schoolListContainer">
+        {schoolList.length === 0 ? (
+          <p style={{ textAlign: "center", fontSize: "30px", marginTop: "5%" }}>
+            등록된 학원이 없습니다.
+          </p>
+        ) : (
+          <ul>
+            {schoolList.map((school) => (
+              <li key={school.id}>{school.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
