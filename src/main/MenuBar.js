40d3cbf0eaa6fbac07c60b98b00ecaa033b4945e
chan <69616036+Qund123@users.noreply.github.com>
2023-11-06 13:22:18 +0900

    icon & ui update

--- src/main/MenuBar.js
+++ src/main/MenuBar.js
@@ -10,16 +10,16 @@ function MenuBar(props) {
       </div>
       <ul>
         <Link to="/main" style={{ color: "white" }}>
-          <li><img src="image/notification.svg" alt="notification" draggable="false" className="icon" />홈</li>
+          <li><img src="image/home.svg" alt="notification" draggable="false" className="icon" />홈</li>
         </Link>
         <Link to="/announcement" style={{ color: "white" }}>
           <li><img src="image/notification.svg" alt="notification" draggable="false" className="icon" />공지사항</li>
         </Link>
         <Link to="/qr" style={{ color: "white" }}>
-          <li><img src="image/notification.svg" alt="notification" draggable="false" className="icon" />출석</li>
+          <li><img src="image/attendance.svg" alt="notification" draggable="false" className="icon" />출석</li>
         </Link>
         <Link to="/signin" style={{ color: "white" }}>
-          <li><img src="image/notification.svg" alt="notification" draggable="false" className="icon" />로그아웃</li>
+          <li><img src="image/log-out.svg" alt="notification" draggable="false" className="icon" />로그아웃</li>
         </Link>
       </ul>
     </div>
